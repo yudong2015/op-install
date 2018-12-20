ff238440ae9805dc132fe82d7c95fdc28f83d312
ferguschen <ferguschen70@163.ccom>
2018-12-20 16:35:26 +0800

    update app-manger

--- app-manager/init.sh
+++ app-manager/init.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
+echo "Start init..." >> /opt/app-manager/log
 
 MYSQL_ROOT_PASSWORD=$(curl -s http://metadata/self/links/external_service/env/openpitrix-db/mysql_root_password)
+echo "MYSQL_ROOT_PASSWORD: ${MYSQL_ROOT_PASSWORD}..." >> /opt/app-manager/log
 
 # migrate app database
 # befor migrating, copy app schema from openpitrix/pkg/schema/app to /opt/app-manager/schema
 docker run -it -d -v /opt/app-manager/schema:/flyway/sql --net=host \
  		   dhoer/flyway:5.1.4-mysql-8.0.11-alpine -url=jdbc:mysql://openpitrix-db/app -user=root -password=${MYSQL_ROOT_PASSWORD} -validateOnMigrate=false migrate
+
+echo "Finished init." >> /opt/app-manager/log
--- app-manager/start.sh
+++ app-manager/start.sh
@@ -1,17 +1,26 @@
 #!/usr/bin/env bash
 
+echo "Start app-manager container..." >> /opt/app-manager/log
+
 CONTAINER_NAME="openpitrix-app-manager"
 LOG_LEVEL=$(curl -s http://metadata/self/env/app_manager_log_level)
 GRPC_SHOW_ERROR_CASE=$(curl -s http://metadata/self/env/openpitrix_grpc_show_error_cause)
 
+echo "LOG_LEVEL: ${LOG_LEVEL}, GRPC_SHOW_ERROR_CASE: ${GRPC_SHOW_ERROR_CASE}." >> /opt/app-manager/log
+
 DB_IP=`cat /opt/app-manager/link-hosts|grep DB_IP|cut -d "=" -f 2`
 DB_HOST=`cat /opt/app-manager/link-hosts|grep DB_HOST|cut -d "=" -f 2`
 
+echo "DB_IP:${DB_IP} DB_HOST:${DB_HOST}" >> /opt/app-manager/log
+
 #Start app-manager container
-docker run -it -d -p 9102:9102 --name ${CONTAINER_NAME} \
-		   -v /opt/app-manager:/opt \
+docker run -it -d -p 9102:9102 -v /opt/app-manager:/opt \
  		   -e OPENPITRIX_LOG_LEVEL=${LOG_LEVEL} \
  		   -e OPENPITRIX_GRPC_SHOW_ERROR_CAUSE=${GRPC_SHOW_ERROR_CASE} \
  		   -e OPENPITRIX_MYSQL_DATABASE=app \
  		   -e OPENPITRIX_PROFILING_ENABLE=1 \
+ 		   --restart=always \
+ 		   --name ${CONTAINER_NAME} \
  		   openpitrix:latest app-manager
+
+echo "App-manager started." >> /opt/app-manager/log
