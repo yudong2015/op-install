663154f0301168ab011af62a84b89992eb02b3c0
ferguschen <ferguschen70@163.ccom>
2018-12-20 18:23:59 +0800

    update op-mysql

--- mysql/start.sh
+++ mysql/start.sh
@@ -5,8 +5,16 @@ CONTAINER_NAME="openpitrix-db"
 
 #Start mysql container
 #Befor starting, you need to copy ddl from openpitrix/pkg/db/ddl to server /opt/db/ddl.
-docker run -it -d -p 3306:3306 --name ${CONTAINER_NAME} -v /opt/db/ddl:/docker-entrypoint-initdb.d -v /opt/db/mysql:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD} mysql:8.0.11
+docker run -it -d -p 3306:3306 \
+		-v /opt/db/ddl:/docker-entrypoint-initdb.d \
+		-v /opt/db/mysql:/var/lib/mysql \
+		-e MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD} \
+		--restart=always \
+		--name ${CONTAINER_NAME} \
+		mysql:8.0.11
+
 #Check mysql container status
 until docker exec ${CONTAINER_NAME} bash -c "echo 'SELECT VERSION();' | mysql -uroot -p${MYSQL_ROOT_PASSWORD}"; do echo "waiting for mysql"; sleep 2; done;
+
 #Init ddl
 until docker exec ${CONTAINER_NAME} bash -c "cat /docker-entrypoint-initdb.d/*.sql | mysql -uroot -p${MYSQL_ROOT_PASSWORD}"; do echo "ddl waiting for mysql"; sleep 2; done;
