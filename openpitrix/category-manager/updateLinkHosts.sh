da56d77bb0b152a1eaff42eba354a8797e032037
ferguschen <ferguschen70@163.ccom>
2018-12-22 23:34:08 +0800

    update category-manger

--- openpitrix/category-manager/updateLinkHosts.sh
+++ openpitrix/category-manager/updateLinkHosts.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
-LOG_FILE="/opt/category-manager/log"
-echo "Start updating link hosts..." >> ${LOG_FILE}
+source /opt/category-manager/log.sh
+log "Start updating link hosts..."
 
 DB_HOST="openpitrix-db"
 ETCD_HOST="openpitrix-etcd"
 DB_IP=`cat /opt/category-manager/link-hosts|grep ${DB_HOST}|cut -d "=" -f 2`
 ETCD_IP=`cat /opt/category-manager/link-hosts|grep ${ETCD_HOST}|cut -d "=" -f 2`
-echo "DB_IP:${DB_IP} DB_HOST:${DB_HOST}" >> ${LOG_FILE}
-echo "ETCD_IP:${ETCD_IP} ETCD_HOST:${ETCD_HOST}" >> ${LOG_FILE}
+log "DB_IP:${DB_IP} DB_HOST:${DB_HOST}"
+log "ETCD_IP:${ETCD_IP} ETCD_HOST:${ETCD_HOST}"
 
 #update local hosts
 ORIGIN_HOSTS="/opt/category-manager/origin-hosts"
@@ -25,8 +25,8 @@ cat ${ORIGIN_HOSTS} ${LINK_HOSTS_TMP} > /etc/hosts
 CONTAINER_NAME="openpitrix-category-manager"
 n=`docker ps|grep ${CONTAINER_NAME}|wc -l`
 if [ $n -gt 0 ]; then
-	echo "Run update container ip..." >> ${LOG_FILE}
+	log "Run update container ip..."
     docker exec -it $CONTAINER_NAME /opt/updateContainerHosts.sh $DB_HOST=$DB_IP $ETCD_HOST=$ETCD_IP
 else
-	echo "There is no container, just update local link hosts..." >> ${LOG_FILE}
+	log "There is no container, just update local link hosts..."
 fi
